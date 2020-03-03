4f5ca20d54041b7f3790eec15c6b649cb3cede8f
Caleb Kavanaugh <calebck@hotmail.com>
2020-03-03 12:52:11 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,9 +1,11 @@
 export function manageFriends(state = {friends: []}, action){
   switch (action.type) {
     case 'ADD_FRIEND':
-      return { friends: [...state.friends, action.friend]}
+      let newState1 = { friends: [...state.friends, action.friend] }
+      return newState1
     case 'REMOVE_FRIEND':
-      return { friends: state.friends.filter(friend => friend.id !== action.id)}
+      let newState2 = { friends: state.friends.filter(friend => friend.id !== action.id) }
+      return newState2
     default:
       return state
   }
